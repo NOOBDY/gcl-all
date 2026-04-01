bc3bcf067023ed807248ce0ec5794288367d2cad
noobdy <htommyf12@gmail.com>
2026-04-01 17:12:22 +0800

    reorganize flake

--- gcl/flake.nix
+++ gcl/flake.nix
@@ -1,45 +1,62 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-      hPkgs = pkgs.haskell.packages."ghc984";
-
-      devTools = with hPkgs; [
-        ghc
-        haskell-language-server
-        ormolu
-
-        stack-wrapped
-        pkgs.zlib
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
       ];
 
-      stack-wrapped = pkgs.symlinkJoin {
-        name = "stack"; # will be available as the usual `stack` in terminal
-        paths = [ pkgs.stack ];
-        buildInputs = [ pkgs.makeWrapper ];
-        postBuild = ''
-          wrapProgram $out/bin/stack \
-            --add-flags "\
-              --no-nix \
-              --system-ghc \
-              --no-install-ghc \
-            "
-        '';
-      };
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          in
+          f {
+            inherit system pkgs;
+            hPkgs = pkgs.haskell.packages."ghc984";
+          }
+        );
     in
     {
-      devShell = pkgs.mkShell {
-        buildInputs = devTools;
+      devShells = forEachSupportedSystem (
+        {pkgs, hPkgs, system}:
+        let
+          devTools = with hPkgs; [
+            ghc
+            haskell-language-server
+            ormolu
 
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devTools;
-      };
-    }
-  );
+            stack-wrapped
+            pkgs.zlib
+          ];
+
+          stack-wrapped = pkgs.symlinkJoin {
+            name = "stack"; # will be available as the usual `stack` in terminal
+            paths = [ pkgs.stack ];
+            buildInputs = [ pkgs.makeWrapper ];
+            postBuild = ''
+              wrapProgram $out/bin/stack \
+                --add-flags "\
+                  --no-nix \
+                  --system-ghc \
+                  --no-install-ghc \
+                "
+            '';
+          };
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            buildInputs = devTools;
+
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devTools;
+          };
+        }
+      );
+    };
 }
