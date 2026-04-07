e504ac2ef5378c4376be6a5d75465a93d7069360
noobdy <htommyf12@gmail.com>
2026-04-07 21:32:26 +0800

    hls working

--- gcl/flake.nix
+++ gcl/flake.nix
@@ -16,55 +16,35 @@
           inherit (haskellNix) config;
         };
 
-        stack-wrapped = pkgs.symlinkJoin {
-          name = "stack"; # will be available as the usual `stack` in terminal
-          paths = [ pkgs.stack ];
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = ''
-            wrapProgram $out/bin/stack \
-              --add-flags "\
-                --no-nix \
-                --system-ghc \
-                --no-install-ghc \
-              "
-          '';
-        };
-
         overlays = [
           haskellNix.overlay
-          (final: prev: {
-            gclProject = final.haskell-nix.stackProject' {
-              src = pkgs.haskell-nix.cleanSourceHaskell {
-                src = ./.;
-                name = "gcl";
-              };
-
-              compiler-nix-name = "ghc984";
-
-              shell = {
-                tools = {
-                  # stack = "3.3.1";
-                  hlint = {};
-                  haskell-language-server = {};
-                  ormolu = {};
-                };
-
-                buildInputs = with pkgs; [
-                  #(pkgs.writeScriptBin "haskell-language-server-wrapper" ''
-                  #  #!${pkgs.stdenv.shell}
-                  #  exec haskell-language-server "$@"
-                  #'')
-                ];
-
-                withHoogle = false;
-              };
-            };
-          })
         ];
 
-        flake = pkgs.gclProject.flake {};
-      in flake // {
-        packages.default = flake.packages."gcl:exe:gcl";
+        project = pkgs.haskell-nix.stackProject' {
+          src = pkgs.haskell-nix.cleanSourceHaskell {
+            src = ./.;
+            name = "gcl";
+          };
+
+          compiler-nix-name = "ghc984";
+        };
+
+      in {
+        packages.default = project.hsPkgs.gcl.components.exes.gcl;
+
+        devShells.default = project.shellFor {
+          tools = {
+            cabal = {};
+            hlint = {};
+            haskell-language-server = {};
+            ormolu = {};
+          };
+
+          buildInputs = [
+          ];
+
+          withHoogle = true;
+        };
       }
     );
 }
